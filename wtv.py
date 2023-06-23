35ef5fdded14bae48ad188104ce39ecaf25e2cec
M-Mbugua <mbuguamichelle9@gmail.com>
2023-06-23 22:04:40 +0000

    Added validation and feedback

--- wtv.py
+++
@@ -1,8 +0,0 @@
-with open("wordlist.txt", "r") as f:
-    names = [line.strip() for line in f.readlines()]
-
-names.sort()
-
-with open("wordlist.txt", "w") as f:
-    for name in names:
-        f.write(name + "\n")
